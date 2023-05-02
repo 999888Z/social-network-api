da28553c37e84fb26c6706fe7910eb11ae5b255a
John <johnwilday@yahoo.com>
2023-05-02 17:32:12 -0600

    Get post and get one routes working

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,38 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create(req.body);
+    res.json(dbUserData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:_id", async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.params._id })
+        //   .select('-__v');
+  
+        if (!user) {
+          return res.status(404).json({ message: 'No user with that ID' });
+        }
+  
+        res.json(user);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+
+});
 
 module.exports = router;
