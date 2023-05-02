6c51a332e19e73b2a9af9701e33dbc5529e79314
John <johnwilday@yahoo.com>
2023-05-02 17:01:51 -0600

    Setup Thought and Reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create Student model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      
+      
+    },
+    username: {
+        type: String,
+      required: true,
+    },
+    reactions: [
+        reactionSchema
+    ]
+
+    
+  
+  },
+  {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+  }
+);
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
+thoughtSchema.virtual('formattedCreatedAt').get(function() {
+    return this.createdAt.toLocaleDateString();
+  });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
