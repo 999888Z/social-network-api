034ec0ba775bad606b2ab824bb5fc3888a1add05
John <johnwilday@yahoo.com>
2023-05-02 18:38:34 -0600

    Added Thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,81 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Thought, User } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:_id", async (req, res) => {
+  try {
+    const thought = await Thought.findOne({ _id: req.params._id });
+
+    if (!thought) {
+      return res.status(404).json({ message: "No thought with that ID" });
+    }
+
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const dbThoughtData = await Thought.create(req.body);
+
+    const userThought = await User.findOneAndUpdate(
+      { _id: req.body.userId },
+      { $addToSet: { thoughts: dbThoughtData._id } },
+
+      { new: true }
+    );
+    res.json(dbThoughtData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:_id", async (req, res) => {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      { _id: req.params._id },
+      { thoughtText: req.body.thoughtText },
+
+      { new: true }
+    );
+
+    if (!thought) {
+      return res
+        .status(404)
+        .json({ message: "No thought found with that ID :(" });
+    }
+
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+    try {
+        const thought = await Thought.findOneAndRemove({ _id: req.params._id });
+  
+        if (!thought) {
+          return res.status(404).json({ message: 'No such thought exists' })
+        }
+  
+   
+  
+        res.json({ message: 'Thought successfully deleted' });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }  
+})
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -22,7 +22,7 @@ router.post("/", async (req, res) => {
 router.get("/:_id", async (req, res) => {
     try {
         const user = await User.findOne({ _id: req.params._id })
-        //   .select('-__v');
+       
   
         if (!user) {
           return res.status(404).json({ message: 'No user with that ID' });
@@ -34,5 +34,59 @@ router.get("/:_id", async (req, res) => {
       }
 
 });
+
+router.put("/:_id", async (req, res) => {
+    try {
+        
+        const user = await User.findOneAndUpdate(
+          { _id: req.params._id },
+          { email: req.body.email,
+            username: req.body.username
+         },
+          {  new: true }
+        );
+  
+        if (!user) {
+          return res
+            .status(404)
+            .json({ message: 'No user found with that ID :(' })
+        }
+  
+        res.json(user);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+
+
+
+
+})
+
+router.delete("/:_id", async (req, res) => {
+    try {
+        const user = await User.findOneAndRemove({ _id: req.params._id });
+  
+        if (!user) {
+          return res.status(404).json({ message: 'No such user exists' })
+        }
+  
+        // const course = await Course.findOneAndUpdate(
+        //   { students: req.params.studentId },
+        //   { $pull: { students: req.params.studentId } },
+        //   { new: true }
+        // );
+  
+        // if (!course) {
+        //   return res.status(404).json({
+        //     message: 'Student deleted, but no courses found',
+        //   });
+        // }
+  
+        res.json({ message: 'User successfully deleted' });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }  
+})
 
 module.exports = router;
